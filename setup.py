131a44cdfd50a6149728a0768d2990b4ac085aff
prateeksingh0001 <prateeksingh0001@gmail.com>
2018-12-06 10:32:40 +0530

    Uploading the entire project

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="DetectorMorse",
+      version="0.2",
+      description="DetectorMorse, a sentence splitter",
+      author="Kyle Gorman",
+      author_email="kylebgorman@gmail.com",
+      packages=["detectormorse"])
